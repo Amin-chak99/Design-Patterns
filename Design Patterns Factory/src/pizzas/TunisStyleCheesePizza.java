fedf5c0fa6a443620fe3cf5cd787d2a9fd5a460a
Amin-chak99 <76822547+Amin-chak99@users.noreply.github.com>
2023-01-07 17:42:12 +0100

    factory

---
+++ Design Patterns Factory/src/pizzas/TunisStyleCheesePizza.java
@@ -0,0 +1,36 @@
+package pizzas;
+
+public class TunisStyleCheesePizza extends Pizza  {
+
+	@Override
+	public void prepare() {
+		System.out.println("pate complet");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bake() {
+		System.out.println("1/2 bake");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cut() {
+		System.out.println("coupé le pizza en 2 tranche");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void box() {
+		System.out.println("Pizza de grande Taille");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
